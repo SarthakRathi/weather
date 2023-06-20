4af762cc02d5f095a13a8bf7090fcd0ada8e086e
SarthakRathi <sarthakrathi89@gmail.com>
2023-06-20 16:38:07 +0530

    changes in display

--- src/components/Search.js
+++ src/components/Search.js
@@ -68,45 +68,43 @@ const Search = () => {
   };
 
   return (
-    <>
-      <ThemeContext.Provider value={{ theme, toggleTheme }}>
-        <div className="container" id={theme}>
-          <div className="weather">
-            <div className="search">
-              <input
-                type="text"
-                placeholder="Enter City Name"
-                onChange={(e) => setName(e.target.value)}
-                onKeyUp={(e) => handleKeyPress(e)}
-              />
-              <button onClick={handleClick}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-search"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-                </svg>
-              </button>
-            </div>
-            {isEditing && (
-              <Winfo
-                image={data.image}
-                celcius={data.celcius}
-                name={data.name}
-                humidity={data.humidity}
-                speed={data.speed}
-              />
-            )}
-            <p>Change Mode</p>{" "}
-            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="container" id={theme}>
+        <div className="weather">
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Enter City Name"
+              onChange={(e) => setName(e.target.value)}
+              onKeyUp={(e) => handleKeyPress(e)}
+            />
+            <button onClick={handleClick}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </button>
           </div>
+          {isEditing && (
+            <Winfo
+              image={data.image}
+              celcius={data.celcius}
+              name={data.name}
+              humidity={data.humidity}
+              speed={data.speed}
+            />
+          )}
+          <p>Change Mode</p>{" "}
+          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
         </div>
-      </ThemeContext.Provider>
-    </>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
